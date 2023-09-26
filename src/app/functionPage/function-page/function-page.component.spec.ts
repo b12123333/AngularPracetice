2dd51fefa2278ed49a501d5b6403877f8e4a9e8d
Barry <barry@wethink.com.tw>
2023-09-26 09:35:58 +0800

    update

---
+++ src/app/functionPage/function-page/function-page.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { FunctionPageComponent } from './function-page.component';
+
+describe('FunctionPageComponent', () => {
+  let component: FunctionPageComponent;
+  let fixture: ComponentFixture<FunctionPageComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ FunctionPageComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(FunctionPageComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
