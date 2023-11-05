c3802994db566470036eaca51fc688e051e8ccaa
Barry <b12123333@gmail.com>
2023-11-06 01:10:01 +0800

    update

--- src/app/login/google-auth.service.ts
+++ src/app/login/google-auth.service.ts
@@ -10,22 +10,6 @@ export class GoogleAuthService {
 
   constructor(private http: HttpClient) { }
 
-  public initiateOAuth() {
-    const authUrl = `https://accounts.google.com/o/oauth2/v2/auth?scope=email%20profile&redirect_uri=${this.redirectUri}&response_type=token&client_id=${this.clientId}`;
-    window.location.href = authUrl;
-  }
-
-  public handleRedirect() {
-    const fragment = window.location.hash.substr(1);
-    const queryParams = new URLSearchParams(fragment);
-    const accessToken = queryParams.get('access_token');
-    if (accessToken) {
-      // You can store the access token and use it to access Google APIs
-      // or make requests to your backend to authenticate the user.
-      console.log(accessToken);
-    }
-  }
-
   oidcLogin(): void {
     const client: google.accounts.oauth2.CodeClient = google.accounts.oauth2.initCodeClient({
       client_id: '537821863898-scj2n8pkm88udb8e40rddsr7b74k941d.apps.googleusercontent.com',
